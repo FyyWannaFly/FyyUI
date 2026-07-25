70a76fcd81848c20e3bdbed2074f7b17775c7b42
FyyWannaFly <fyywannafly@gmail.com>
2026-07-25 19:27:19 +0700

    build drag surface bounds

--- fyyui.lua
+++ fyyui.lua
@@ -4762,7 +4762,7 @@ return (function()
 		})
 		self.TopbarDragSurface = U.Create("TextButton", {
 			Name = "DragSurface",
-			Size = UDim2.new(1, 0, 1, 0),
+			Size = UDim2.new(1, -160, 1, 0),
 			BackgroundTransparency = 1,
 			BorderSizePixel = 0,
 			Text = "",
